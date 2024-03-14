c8f086b0e85adb106d01e141dd8c1fab339cf392
Ahfazgfra <ahfazgafara86@gmail.com>
2024-03-14 15:33:05 +0700

    first commit

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,7 @@
+{{-- @props(['options', 'name', 'selected' => ''])
+<select name="{{ $name }}" id="{{ $name }}">
+    @foreach ($options as $key => $value)
+        <option value="{{ $key }}" @if ($key === $selected) selected @endif>{{ $value }}
+        </option>
+    @endforeach
+</select> --}}
